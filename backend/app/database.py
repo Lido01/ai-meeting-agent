c73176f6cabc40b77b353a2e0195dc6dc4c11752
Lidetu <lidosinafanis@gmail.com>
2026-08-14 01:51:49 +0300

    database updates for user can send data to postgress and connect

--- backend/app/database.py
+++ backend/app/database.py
@@ -17,3 +17,13 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
