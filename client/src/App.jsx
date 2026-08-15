35a6fda2e8ddd59f909b8f7ef07342d069d3e643
Lidetu <lidosinafanis@gmail.com>
2026-08-15 14:43:34 +0300

    front main configuration files track

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          AI Powered Meeting Assistant
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
